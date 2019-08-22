ec6859783e8905f07025fa0b2845dc359787d4d5
Sakar Malla <Sakar-Malla@live.nmit.ac.nz>
2019-08-23 08:48:09 +1200

    Add project files.

---
+++ CinemaVBApp/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form
+    Public Function Adult_Before_5(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_price As Double = 14.5
+        Dim total_Price = Actual_price * quantity
+        If time <= 5 Then
+            Return total_Price
+        Else Return 0
+        End If
+
+    End Function
+    Public Function Adult_After_5(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_price As Double = 17.5
+        Dim total_Price = Actual_price * quantity
+        If time >= 5 Then
+            Return total_Price
+        Else Return 0
+        End If
+    End Function
+
+    Public Function Adult_Tuesday_Allday(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_Price As Double = 13
+        Dim total_price = Actual_Price * quantity
+        If day = "tuesday" Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Public Function Child_Under16_Allday(quantity As Integer, person As String, day As String, time As Decimal, Age As Double) As Double
+        Dim Actual_Price As Double = 12
+        Dim total_price = Actual_Price * quantity
+        If Age <= 16 Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Public Function Senior_65andAbove_Allday(quantity As Integer, person As String, day As String, time As Decimal, Age As Double) As Double
+        Dim Actual_Price As Double = 12.5
+        Dim total_price = Actual_Price * quantity
+        If Age >= 65 Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Public Function Student_Allday(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_Price As Double = 14.0
+        Dim total_price = Actual_Price * quantity
+        If person = "Student" Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Public Function Family_Allday(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_Price As Double = 46.0
+        Dim total_price = Actual_Price * quantity
+        If person = "Family" Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Public Function Chick_Flick_Thrusday(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_Price As Double = 21.5
+        Dim total_price = Actual_Price * quantity
+        If day = "Thursday" And person = "ChickFlick" Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Public Function Kids_Carers_Wednesday(quantity As Integer, person As String, day As String, time As Decimal) As Double
+        Dim Actual_Price As Double = 12.0
+        Dim total_price = Actual_Price * quantity
+        If day = "Wednesday" And person = "KidsandCarers" Then
+            Return total_price
+        Else Return 0
+        End If
+    End Function
+    Private Sub Cinemas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ UnitTestProject1/UnitTest1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports CinemaVBApp
+
+<TestClass()> Public Class UnitTest1
+    <TestMethod()> Public Sub Adult_Before_5_Test()
+        Dim form1 As Form = New Form
+        Dim adult_before_5 = form1.Adult_Before_5(2, "person", "wednesday", 4)
+
+        Assert.AreEqual(29.0, adult_before_5)
+    End Sub
+    <TestMethod()> Public Sub Adult_After_5_Test()
+        Dim form1 As Form = New Form
+        Dim adult_before_5 = form1.Adult_After_5(2, "person", "wednesday", 6)
+        Assert.AreEqual(35.0, adult_before_5)
+    End Sub
+    <TestMethod()> Public Sub Adult_tuesday_Allday_Test()
+        Dim form1 As Form = New Form
+        Dim Adult_Tuesday_Allday = form1.Adult_Tuesday_Allday(2, "person", "tuesday", 6)
+        Assert.AreEqual(26.0, Adult_Tuesday_Allday)
+    End Sub
+    <TestMethod()> Public Sub Child_Under16_allday_test()
+        Dim form1 As Form = New Form
+        Dim Child_Under16_Allday = form1.Child_Under16_Allday(1, "person", "tuesday", 6, 12)
+        Assert.AreEqual(12.0, Child_Under16_Allday)
+    End Sub
+    <TestMethod()> Public Sub Senior_65andAbove_test()
+        Dim form1 As Form = New Form
+        Dim senior_65andAbove_Allday = form1.Senior_65andAbove_Allday(1, "person", "tuesday", 6, 67)
+        Assert.AreEqual(12.5, senior_65andAbove_Allday)
+    End Sub
+    <TestMethod()> Public Sub Student_Allday_test()
+        Dim form1 As Form = New Form
+        Dim Student_Allday = form1.Student_Allday(2, "Student", "Monday", 6)
+        Assert.AreEqual(28.0, Student_Allday)
+    End Sub
+    <TestMethod()> Public Sub Family_Allday_test()
+        Dim form1 As Form = New Form
+        Dim Family_Allday = form1.Family_Allday(2, "Family", "Monday", 6)
+        Assert.AreEqual(92.0, Family_Allday)
+    End Sub
+    <TestMethod()> Public Sub Chick_Flcik_thursday_test()
+        Dim form1 As Form = New Form
+        Dim Chick_Flick_Thursday = form1.Chick_Flick_Thrusday(1, "ChickFlick", "Thursday", 6)
+        Assert.AreEqual(21.5, Chick_Flick_Thursday)
+    End Sub
+    <TestMethod()> Public Sub Kids_Carers_Wednesday_test()
+        Dim form1 As Form = New Form
+        Dim Kids_Carers_Wednesday = form1.Kids_Carers_Wednesday(1, "KidsandCarers", "Wednesday", 6)
+        Assert.AreEqual(12.0, Kids_Carers_Wednesday)
+    End Sub
+End Class
